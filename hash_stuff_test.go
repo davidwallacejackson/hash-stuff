d373c87dee442e9ba5dc114169c1bf8198ed66a4
David Jackson <davidjackson@Davids-MacBook-Pro.local>
2020-12-09 22:00:29 -0800

    First test

--- hash_stuff_test.go
+++ hash_stuff_test.go
@@ -2,22 +2,59 @@ package hash_stuff
 
 import (
 	"io/ioutil"
+	"os"
 	"path/filepath"
+	"sort"
 	"strconv"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
 )
 
-func generateFiles() (string, error) {
+func generateTmpDir() (string, []string, error) {
 	dirName, err := ioutil.TempDir("./tmp", "test-")
 	if err != nil {
-		return "", err
+		return "", nil, err
 	}
+
+	files, err := generateFiles(dirName)
+	if err != nil {
+		return "", nil, err
+	}
+
+	return dirName, files, nil
+}
+
+func generateFiles(dirName string) ([]string, error) {
+	files := []string{}
 
 	for i := 0; i < 1000; i++ {
 		strI := strconv.Itoa(i)
-		if err := ioutil.WriteFile(filepath.Join(dirName, strI), []byte("test "+strI), 0777); err != nil {
-			return "", err
+		path := filepath.Join(dirName, strI)
+		if err := ioutil.WriteFile(path, []byte("content"), 0777); err != nil {
+			return nil, err
 		}
+
+		files = append(files, path)
 	}
 
-	return dirName, nil
+	sort.Strings(files)
+	return files, nil
+}
+
+func TestListFiles(t *testing.T) {
+	dirName, actualFiles, err := generateTmpDir()
+	defer os.RemoveAll(dirName)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	foundFiles, err := listFiles(dirName, []string{"**"}, []string{})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if diff := cmp.Diff(foundFiles, actualFiles); diff != "" {
+		t.Fatal("listFiles output should contain all files in temp dir", diff)
+	}
 }
